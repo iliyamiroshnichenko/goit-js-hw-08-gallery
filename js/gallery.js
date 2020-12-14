f8dd711e0714312f91d8765ce3cd5e461faae644
Iliya <iliyamiroshnichenko88@gmail.com>
2020-12-14 19:45:08 +0200

    Создание и рендер разметки

--- js/gallery.js
+++ js/gallery.js
@@ -1,2 +1,27 @@
 import gallery from "./gallery-items.js";
-// console.table(gallery);
+
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+const refs = {
+  galleryList: document.querySelector(".js-gallery"),
+};
+
+const createElement = ({ preview, original, description }) => {
+    return refs.galleryList.insertAdjacentHTML(
+      "beforeend",
+      `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href=${original}
+  >
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`
+    );
+}
+
+gallery.map((image) => createElement(image));
