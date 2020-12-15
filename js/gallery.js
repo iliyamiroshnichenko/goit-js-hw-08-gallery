a8be34d6d0e3e0cf38a260bccfc36b56b1229a63
Iliya <iliyamiroshnichenko88@gmail.com>
2020-12-15 17:00:47 +0200

    Открытие и закрытие модалки

--- js/gallery.js
+++ js/gallery.js
@@ -3,12 +3,15 @@ import gallery from "./gallery-items.js";
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
 const refs = {
   galleryList: document.querySelector(".js-gallery"),
+  lightBox: document.querySelector(".js-lightbox"),
+  bigImg: document.querySelector(".lightbox__image"),
+  closeBtn: document.querySelector(".lightbox__button"),
 };
 
-const createElement = ({ preview, original, description }) => {
-    return refs.galleryList.insertAdjacentHTML(
-      "beforeend",
-      `<li class="gallery__item">
+const createElement = ({ preview, original, description }) =>
+  refs.galleryList.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item">
   <a
     class="gallery__link"
     href=${original}
@@ -17,11 +20,41 @@ const createElement = ({ preview, original, description }) => {
       class="gallery__image"
       src=${preview}
       data-source=${original}
-      alt=${description}
+      alt='${description}'
     />
   </a>
 </li>`
-    );
-}
+  );
 
 gallery.map((image) => createElement(image));
+
+//Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+refs.galleryList.addEventListener("click", onGalleryClick);
+refs.closeBtn.addEventListener('click', onCloseModal);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  const imgRef = event.target;
+  if (imgRef.nodeName !== "IMG") {
+    return;
+  }
+  const bigImgURL = imgRef.dataset.source;
+  setBigImgSrc(bigImgURL, imgRef.alt);
+  onOpenModal();
+}
+
+function setBigImgSrc(url, alt) {
+    refs.bigImg.src = url;
+    refs.bigImg.alt = alt;
+}
+
+function onOpenModal() {
+  refs.lightBox.classList.add("is-open");
+}
+function onCloseModal() {
+  refs.lightBox.classList.remove("is-open");
+  refs.bigImg.src = '';
+  refs.bigImg.alt = '';
+}
+
+
